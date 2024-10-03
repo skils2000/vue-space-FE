d39648b71ad3d6b196cd9d2c42be9642655579d6
Ilya Skorokhodov <skorox2000@gmail.com>
2024-10-03 05:43:17 +0300

    Add styles to Button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button @click="handleClick" :disabled="disabled">
+    <button class="button" @click="handleClick" :disabled="disabled">
         <slot></slot>
     </button>
 </template>
@@ -19,3 +19,24 @@ export default {
     }
 };
 </script>
+
+<style scoped>
+.button {
+    background-color: #007bff;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    padding: 10px 16px;
+    cursor: pointer;
+    font-size: 16px;
+}
+
+.button:hover {
+    background-color: #0056b3;
+}
+
+.button:disabled {
+    background-color: #c0c0c0;
+    cursor: not-allowed;
+}
+</style>
