cc3109ddd41ae9ed82667e8ad5671facfb653513
Ilya Skorokhodov <skorox2000@gmail.com>
2024-10-03 05:45:35 +0300

    Cleaned the project

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import ApplicationView from '../views/ApplicationView.vue';
 import ApplicationManager from '@/components/ApplicationManager.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/applications',
-      name: 'Applications',
-      component: ApplicationView,
-    },
     {
       path: '/apps',
       name: 'apps',
